8c0783f6ab6f55a5dbde17a694e09e8b2aaf792d
Balaji Venkatraman <rslbalaji@gmail.com>
2022-08-04 21:54:36 +0530

    Add location

--- src/resources/constants.js
+++ src/resources/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   mapLocation: 'https://goo.gl/maps/W7gJQfZXKzLmSv3S9',
-  engagementHallName: 'Amman Thirumana mandapam',
+  engagementHallName: 'Amman Thirumana mandapam, Pochampalli, Krishnagiri',
   engagementDate: '14th',
   engagementTime: '10:30AM',
 };
